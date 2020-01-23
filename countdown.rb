ce2eafbc5b2cb2eb143d4de04001a9b0cc10adea
Matt Kuras <mattkuras@gmail.com>
2020-01-23 22:37:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,5 @@
 
 
-def countdown(number)
-  counter = 10 
-  while counter > 0 
-  puts "#{counter} SECOND(S)!"
-  counter -= 1 
-
-end
-  return "HAPPY NEW YEAR!"
-end
 
 def countdown_with_sleep
   counter = 10 
